6853e5d2b08fad2a98609509492a7341995c2cf1
emmanuelroydelachaise-lang <emmanuel.roydelachaise@gmail.com>
2026-09-14 23:26:23 +0200

    Ajoute le service worker PWA

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = 'ljs-heures-v9';
+const ASSETS = ['./','./index.html','./styles.css','./config.js','./app-1.js','./app-2.js','./app-3.js','./app-4.js','./app-5.js','./app-6.js','./manifest.webmanifest','./icon-192.png','./icon-512.png','./ljs-logo.png','./print-template.png'];
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
+self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(fetch(e.request).then(r => {
+    const copy = r.clone();
+    caches.open(CACHE).then(c => c.put(e.request, copy));
+    return r;
+  }).catch(() => caches.match(e.request)));
+});
