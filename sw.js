8fbab31c6b16fdf872a876ada6a8d8effceff5e5
emmanuelroydelachaise-lang <emmanuel.roydelachaise@gmail.com>
2026-09-15 07:41:38 +0200

    Force le nouveau logo dans le cache PWA

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ljs-heures-v14';
+const CACHE = 'ljs-heures-v15';
 const ASSETS = ['./','./index.html','./styles.css','./print-vector.css','./config.js','./app-1.js','./app-2.js','./app-3.js','./app-4.js','./app-5.js','./app-6.js','./admin-reset.js','./ui-labels.js','./admin-projects.js','./manifest.webmanifest','./brand.svg','./print-template.svg'];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
 self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
