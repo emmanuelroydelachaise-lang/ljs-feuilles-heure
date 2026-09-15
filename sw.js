b7aaa2ba35e59cdb79a200c66d106a2c246a96c4
emmanuelroydelachaise-lang <emmanuel.roydelachaise@gmail.com>
2026-09-15 05:30:11 +0200

    Force le nouveau modèle d'impression

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ljs-heures-v10';
+const CACHE = 'ljs-heures-v11';
 const ASSETS = ['./','./index.html','./styles.css','./config.js','./app-1.js','./app-2.js','./app-3.js','./app-4.js','./app-5.js','./app-6.js','./manifest.webmanifest','./brand.svg','./print-template.png'];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
 self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
