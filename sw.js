0466b17423ea05a8b7e77ab589a64a0c6b42645a
emmanuelroydelachaise-lang <emmanuel.roydelachaise@gmail.com>
2026-09-15 18:51:19 +0200

    Cache responsible vehicle management

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE = 'ljs-heures-v26';
+const CACHE = 'ljs-heures-v27';
 const ASSETS = [
   './', './index.html', './styles.css', './print-vector.css', './print-fit.css', './admin-archives.css', './config.js',
   './app-1.js', './app-2.js', './app-3.js', './app-4.js', './app-5.js', './app-6.js',
-  './admin-reset.js', './ui-labels.js', './admin-projects.js', './interim-agency.js', './technician-state.js', './admin-pin-guard.js', './admin-archives.js', './pwa-clean.js', './print-logo-fix.js', './print-ready-fix.js',
+  './admin-reset.js', './ui-labels.js', './admin-projects.js', './interim-agency.js', './technician-state.js', './admin-vehicles.js', './admin-pin-guard.js', './admin-archives.js', './pwa-clean.js', './print-logo-fix.js', './print-ready-fix.js',
   './manifest.webmanifest', './brand.png', './icon-192-v2.png', './icon-512-v2.png', './print-template.svg'
 ];
 
