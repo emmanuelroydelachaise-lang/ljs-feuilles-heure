3800825bfe9c4022a66119477f3eadfc00269515
emmanuelroydelachaise-lang <emmanuel.roydelachaise@gmail.com>
2026-09-15 06:23:20 +0200

    Force le nouveau RAZ et le libellé Absent

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'ljs-heures-v12';
-const ASSETS = ['./','./index.html','./styles.css','./print-vector.css','./config.js','./app-1.js','./app-2.js','./app-3.js','./app-4.js','./app-5.js','./app-6.js','./admin-reset.js','./manifest.webmanifest','./brand.svg','./print-template.svg'];
+const CACHE = 'ljs-heures-v13';
+const ASSETS = ['./','./index.html','./styles.css','./print-vector.css','./config.js','./app-1.js','./app-2.js','./app-3.js','./app-4.js','./app-5.js','./app-6.js','./admin-reset.js','./ui-labels.js','./manifest.webmanifest','./brand.svg','./print-template.svg'];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
 self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', e => {
